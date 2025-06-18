9c1f7752f92b287b260ed4fdb985ec04af3b0492
Gugustinette <63470020+Gugustinette@users.noreply.github.com>
2025-06-18 15:47:11 +0300

    feat: config unocss with tsdown (#1)

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'tsdown'
 import Vue from 'unplugin-vue/rolldown'
+import unocssRollupPlugin from './plugins/unocss-rollup-plugin.mjs'
 
 export default defineConfig([
   {
     entry: ['./src/index.ts'],
     platform: 'neutral',
-    plugins: [Vue({ isProduction: true })],
+    plugins: [Vue({ isProduction: true }), unocssRollupPlugin()],
     dts: { vue: true },
   },
 ])
