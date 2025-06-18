9ae42c9ae36b4d6628d05a89b8d785eb41fc8cea
npldevfr <asnpldev@gmail.com>
2025-06-18 02:36:42 +0200

    refactor: Update eslint configuration with storybook plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,6 @@
-// import storybook from "eslint-plugin-storybook";
 import { sxzz } from '@sxzz/eslint-config'
+import storybook from 'eslint-plugin-storybook'
 
 export default sxzz()
+  .removeRules('import/no-default-export')
+  .append({ ...storybook.configs.all })
